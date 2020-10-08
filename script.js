85748a4afcdbb61fd20d5f8a0b89617e76c08630
Grant Chang <granthchang01@gmail.com>
2020-10-08 09:58:20 -0600

    Improved accuracy, added digital clock

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 setInterval(setClock, 10)
 
+// Object references
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
+const hourDig = document.getElementById('digital-hour')
+const minuteDig = document.getElementById('digital-minute')
+const secondDig = document.getElementById('digital-second')
+
+// Set clocks
 function setClock() {
+  
+  // Calculating day percentage
   const currentDate = new Date()
   const totalSeconds = (currentDate.getHours() * 3600) + (currentDate.getMinutes() * 60) + currentDate.getSeconds()
-  const dayPercent = (totalSeconds / 86400) //+ ( (currentDate.getMilliseconds() % 1000) / 1000)
+  const totalMS = totalSeconds * 1000 + currentDate.getMilliseconds();
+  const dayPercent = totalMS / 86400000;
 
-  const secondsRatio = ( (100000 * dayPercent) % 100) / 100
-  const minutesRatio = ( (1000 * dayPercent) % 100) / 100
-  const hoursRatio = dayPercent;
+  const secondRatio = (dayPercent * 1000) % 1
+  const minuteRatio = (dayPercent * 10) % 1
+  const hourRatio = dayPercent;
 
-  setRotation(secondHand, secondsRatio)
-  setRotation(minuteHand, minutesRatio)
-  setRotation(hourHand, hoursRatio)
+  // Update analog clock
+  setRotation(secondHand, secondRatio)
+  setRotation(minuteHand, minuteRatio)
+  setRotation(hourHand, hourRatio)
+
+  // Update digital clock
+  setNumber(hourDig, hourRatio * 10)
+  setNumber(minuteDig, minuteRatio * 100)
+  setNumber(secondDig, secondRatio * 100)
 }
 
+// Rotate analog clock hands
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * 360)
 }
 
+// Set the number for a given element
+function setNumber(element, value) {
+  if (value < 10) {
+    element.innerHTML = '0'
+  } else {
+    element.innerHTML = '';
+  }
+  element.innerHTML += Math.floor(value);
+}
+
+// Update on page open
 setClock()
